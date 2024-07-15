27946eed787a0903bad23066811b2750af4deddc
vuanhtuan17072005 <vuanhtuan17072005@gmail.com>
2024-07-15 18:16:43 +0700

    btvn-1

---
+++ buổi1/tổng các số 1,n.py
@@ -0,0 +1,8 @@
+print("Nhập một số N bất kì :")
+n=int(input())
+s=0
+i=1
+while i<=n :
+    s=s+i
+    i=i+1
+print("S =",s)
---
+++ buổi1/while 1-10.py
@@ -0,0 +1,5 @@
+print("Nhập một số bất kì")
+x=-1
+while x<1 or x>10 :
+    x=int(input("Mời bạn nhập vào 1 số trong khoảng [1,10] :"))
+print("x = ",x**2)
